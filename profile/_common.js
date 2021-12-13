2d51b7aa8892443e05b6ba3a113cd129f11158a9
Michael-Daniel Borca <m.borca@tiffinger-thiel.de>
2021-12-13 16:11:55 +0100

    add react hook rules and remove Interface I-prefix

--- profile/_common.js
+++ profile/_common.js
@@ -13,13 +13,16 @@ function buildRules(profile) {
 
       "plugin:prettier/recommended"
     ],
+    plugins: ["react-hooks"],
     parserOptions: {
       project: "./tsconfig.json"
     },
     rules: {
       "no-void": "off",
       ...(profile === "react" ? {
-        "react/no-danger": "warn"
+        "react/no-danger": "warn",
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
       } : {}),
 
       ...(profile === "nest" ? {
@@ -40,14 +43,12 @@ function buildRules(profile) {
         // Pass all original configs and edit them if needed:
         ...require('@rushstack/eslint-config/profile/web-app').overrides[0].rules['@typescript-eslint/naming-convention'].slice(1).map((rule) => {
           
-          if (profile === "react") {
-            // Disable the need for an 'I' prefix of interfaces.
-            if (rule.selector === "interface" && rule.custom.regex === '^_?I[A-Z]' ) {
-              rule.custom.regex = '^_?[A-Z]';
-              return rule;
-            }
+          // Disable the need for an 'I' prefix of interfaces.
+          if (rule.selector === "interface" && rule.custom.regex === '^_?I[A-Z]' ) {
+            rule.custom.regex = '^_?[A-Z]';
+            return rule;
           }
-          
+        
           // Do not force underscores if private:
           if (rule.modifiers?.length > 0 && rule.modifiers[0] === "private" && rule.leadingUnderscore === 'require' ) {
             rule.leadingUnderscore = "allow";
